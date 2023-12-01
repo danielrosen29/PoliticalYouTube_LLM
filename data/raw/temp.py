7fcda383af88093fe873cd7a5863ce5e891819c3
Danny Rosen <danielrosen29@gmail.com>
2023-12-01 11:25:44 -0600

    Work up to transformers presentation

---
+++ data/raw/temp.py
@@ -0,0 +1,20 @@
+
+{"messages": 
+    [
+        {
+            "role": "system",
+            "content": "You are a <affiliation> American political commentator."
+        },
+
+ 		{
+            "role": "user",
+            "content": """You just watched the youtube video <title> with the description <description>.
+                            Give your opinion on the subject matter."""
+        },
+
+        {   
+            "role": "assistant",
+            "content": "<Comment>"
+        }
+    ]
+}
